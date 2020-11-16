1b6fd83babf4bdcc56564e02cb7c9faa27ea2137
Mikkel Agerlin Christensen <mikkel_ac@hotmail.com>
2020-11-16 17:07:31 +0100

    Inverted indexes

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import csv
 being_question_indexes = [8, 9, 11, 12, 13, 15, 16, 17, 18, 20, 21, 23, 24, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36]
 # Indexes in the CSV file which are answers to questions about DOING agile
 doing_question_indexes = [10, 14, 19, 22, 25]
+# Indexes in the CSV file where the answers are asked inverted (eg. DONT you instead of DO you)
+inverted_question_indexes = [13, 24, 27, 33, 36]
 
 
 def main():
@@ -11,12 +13,16 @@ def main():
     company_being = 0
     with open(file_path, newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        num_answers = 0
         for (index, row) in enumerate(reader):
+            num_answers += 1
             if(index > 0): # first row is headers
                 doing_score, being_score = get_answer_score(row)
                 company_doing += doing_score
                 company_being += being_score
-                print(f"Company scored - Doing: {company_doing}, Being: {company_being}. Disparity: {abs(company_being - company_doing)}")
+    max_doing = num_answers * len(doing_question_indexes)
+    max_being = num_answers * len(being_question_indexes) * 2 # 2 pts for answering "STRONGLY agree"
+    print(f"Company scored - Doing: {company_doing}/{max_doing}, Being: {company_being}/{max_being}. Disparity: {abs(company_being - company_doing)}")
 
 
 def get_answer_score(answer_data):
@@ -26,7 +32,7 @@ def get_answer_score(answer_data):
         if(index in doing_question_indexes):
             doing_score += 1 if "yes" in answer.lower() else 0
         elif(index in being_question_indexes):
-            being_score += likert_to_int(answer)
+            being_score += (-1 * likert_to_int(answer)) if index in inverted_question_indexes else likert_to_int(answer)
     return (doing_score, being_score)
 
 def likert_to_int(likert_val):
